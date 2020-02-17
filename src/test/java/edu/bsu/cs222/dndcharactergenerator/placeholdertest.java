eee1ddd9bcc83e02736bef992f11bacb01daaf28
TheLuckyRogue <spencernaugler7@gmail.com>
2020-02-17 17:34:32 -0500

    edit build.gradle for javafx and junit 5
    get tests to compile

--- src/test/java/edu/bsu/cs222/dndcharactergenerator/placeholdertest.java
+++ src/test/java/edu/bsu/cs222/dndcharactergenerator/placeholdertest.java
@@ -1,4 +1,10 @@
 package edu.bsu.cs222.dndcharactergenerator;
 
+import org.junit.jupiter.api.Test;
+
 public class placeholdertest {
+    @Test
+    public void testjavafx() {
+        System.out.println("Hello World");
+    }
 }
