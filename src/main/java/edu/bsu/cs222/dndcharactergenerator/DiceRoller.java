0e30b34691de8e5da0a238cdbc61c54d9aa13291
Joshua Kennedy <saxkennedy@gmail.com>
2020-02-19 16:00:42 -0500

    Add/Finalize DiceRoller Add/Start Scene 3

---
+++ src/main/java/edu/bsu/cs222/dndcharactergenerator/DiceRoller.java
@@ -0,0 +1,25 @@
+package edu.bsu.cs222.dndcharactergenerator;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class DiceRoller {
+
+    public ArrayList<Integer> getStats() {
+        ArrayList<Integer> stats = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            ArrayList<Integer> sorter = new ArrayList<Integer>();
+            sorter.add((int)(Math.random()*6+1));
+            sorter.add((int)(Math.random()*6+1));
+            sorter.add((int)(Math.random()*6+1));
+            sorter.add((int)(Math.random()*6+1));
+            Collections.sort(sorter);
+            sorter.remove(0);
+            int sum = sorter.get(0) + sorter.get(1) + sorter.get(2);
+            stats.add(sum);
+        }
+        return stats;
+    }
+}
--- src/main/java/edu/bsu/cs222/dndcharactergenerator/View.java
+++ src/main/java/edu/bsu/cs222/dndcharactergenerator/View.java
@@ -1,5 +1,4 @@
 package edu.bsu.cs222.dndcharactergenerator;
-import javafx.collections.FXCollections;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -7,16 +6,12 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
-import org.graalvm.compiler.phases.common.NodeCounterPhase;
-
-import java.sql.Array;
-import java.util.List;
 
 public class View extends Application {
 
     public static void main(String[] args) {launch((args));}
     @Override
-    public void start(Stage stage){
+    public void start(Stage stage) {
         Character character = new Character();
 
         //First Scene
@@ -24,19 +19,20 @@ public class View extends Application {
         Label enterName = new Label("Enter your name below, bold adventurer!  Then press 'GO!'\n\n *Leaving this blank is fine, you can always change it later!");
         TextField characterName = new TextField();
         Button go = new Button("GO!");
-        Scene scene0=new Scene(vbox0,400,400);
+        Scene scene0=new Scene(vbox0,600,600);
         vbox0.getChildren().addAll(enterName,characterName,go);
 
 
         //Second Scene
         VBox vbox1= new VBox();
-        Scene scene1=new Scene(vbox1, 400,400);
+        Scene scene1=new Scene(vbox1, 600,600);
         Label raceSelection=new Label("Choose from one of the below races!");
         Label statGeneration = new Label("Fill out your rolled stats below.\nWe have provided a dice roller that uses standard rules,\nFeel free to insert your own values, or take ours!");
-
+        Button nextToRacial =new Button("Next");
         Button diceRoller=new Button("Roll the Dice!");
 
         Integer[] statNums = new Integer[]{3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22};
+        Label rolledStats=new Label("");
         Label strLabel=new Label("Strength");
         ComboBox strBox=new ComboBox();
         strBox.getItems().addAll(statNums);
@@ -60,8 +56,12 @@ public class View extends Application {
         ComboBox races = new ComboBox();
         Button back = new Button("Back (Name Screen)");
         races.getItems().addAll("Dragonborn","Dwarf","Elf","Gnome","Half-Elf","Halfling","Half-Orc","Human","Tiefling");
-        vbox1.getChildren().addAll(raceSelection,races,statGeneration,strLabel,strBox,dexLabel,dexBox,conLabel,conBox,intLabel,intBox,wisLabel,wisBox,chaLabel,chaBox,back);
+        vbox1.getChildren().addAll(raceSelection,races,statGeneration,diceRoller,rolledStats,strLabel,strBox,dexLabel,dexBox,conLabel,conBox,intLabel,intBox,wisLabel,wisBox,chaLabel,chaBox,nextToRacial,back);
 
+        //Scene 3
+        VBox vbox2= new VBox();
+        Scene scene2=new Scene(vbox2, 600,600);
+        Label racialAttributes=new Label("Racial Options!");
 
         //GO! button
         go.setOnAction(actionEvent-> {
@@ -78,13 +78,36 @@ public class View extends Application {
         back.setOnAction(actionEvent->{
             stage.setScene(scene0);
         });
+        nextToRacial.setOnAction(actionEvent->{
+            stage.setScene(scene2);
+        });
 
         //Dice Roller Button
         diceRoller.setOnAction(actionEvent->{
+            //character.setStats();
+            rolledStats.setText(character.statRoll().toString());
             //Populate dice rolls.  This is where I stopped 2/18/2020 Joshua Kennedy
         });
 
-
+        //Stat Buttons
+        strBox.setOnAction(actionEvent->{
+            character.setSTR((int)strBox.getValue());
+        });
+        dexBox.setOnAction(actionEvent->{
+            character.setDEX((int)dexBox.getValue());
+        });
+        conBox.setOnAction(actionEvent->{
+            character.setCON((int)conBox.getValue());
+        });
+        intBox.setOnAction(actionEvent->{
+            character.setINT((int)intBox.getValue());
+        });
+        wisBox.setOnAction(actionEvent->{
+            character.setWIS((int)wisBox.getValue());
+        });
+        chaBox.setOnAction(actionEvent->{
+            character.setCHA((int)chaBox.getValue());
+        });
 
 
         stage.setScene(scene0);
