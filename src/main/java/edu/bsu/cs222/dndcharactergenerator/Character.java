a2241c74d373735c529f8cc84514be9282ec7d77
TheLuckyRogue <spencernaugler7@gmail.com>
2020-02-19 23:33:59 -0500

    error handling for setRace

--- src/main/java/edu/bsu/cs222/dndcharactergenerator/Character.java
+++ src/main/java/edu/bsu/cs222/dndcharactergenerator/Character.java
@@ -1,6 +1,8 @@
 package edu.bsu.cs222.dndcharactergenerator;
 
 
+import com.sun.media.sound.InvalidDataException;
+
 import java.util.ArrayList;
 
 public class Character {
@@ -107,10 +109,13 @@ public class Character {
         return race;
     }
 
-    public void setRace(String race) {
+    public void setRace(String race) throws InvalidDataException{
         if (this.race != null) {
             System.out.println("Old race bonus removed");
             this.race.removeRaceModifier(this);
+        }
+        else {
+            throw new InvalidDataException();
         }
 
         Race enumRace = assignEnumRace(race);
